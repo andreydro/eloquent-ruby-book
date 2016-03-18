f041fa239bbbbcad0b5b2112697c28828b70db5c
dro14 <and-rey-2309@mail.ru>
2016-03-18 13:01:01 +0300

    Add 1 chapter

---
+++ 1ch.rb
@@ -0,0 +1,61 @@
+class Document
+  attr_accessor :title, :author, :content
+
+  def initialize(title, author, content)
+    @title = title
+    @author = author
+    @content = content
+  end
+
+  def words
+    @content.split
+  end
+
+  def word_count
+    words.size
+  end
+end
+
+return 0 if divisor == 0
+
+count += 1
+
+def count_words_in(the_string)
+  the_words = the_string.split
+  the_words.size
+end
+
+FurlongsPerFornight = 0.0001663
+
+ANTLERS_PER_MALE_NIISE = 2
+
+def find_document(title, author)
+  # body omitted...
+end
+
+find_document("Frankenstein", "Shelley")
+
+puts "Look Ma,  no parentheses"
+
+def words
+  @content.split
+end
+
+if words.size < 100
+  puts "The document is not very long"
+end
+
+20.times do |n|
+  puts "The number is #{n}"
+  puts "Twice the numberis #{n*2}"
+end
+
+doc.words.each { |word| puts word }
+
+puts doc.author
+
+doc.instance_of? Document
+
+puts do.instance_of?( self.class.superclass.class)
+
+pi = Float("3.14159")
